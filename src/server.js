18f3019250ced26a8e3494d13823df28745bf16f
RossPerot <rmh8980@g.rit.edu>
2017-09-25 13:50:02 -0400

    finished

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const index = fs.readFileSync(`${__dirname}/../client/index.html`);
+
+const onRequest = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const app = http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
+
+const io = socketio(app);
+
+let score = 0;
+
+io.on('connection', (socket) => {
+  socket.join('room1');
+
+  socket.on('updateScore', (data) => {
+    score += data;
+    socket.emit('updatedScore', score);
+  });
+});
